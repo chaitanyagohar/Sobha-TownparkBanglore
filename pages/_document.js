d095d706ed22abd1c6fabbc1b95834400e72263d
chaitanyagohar <chaitanyagohar@gmail.com>
2025-09-21 11:16:22 +0530

    This is to remove the GTM and add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,59 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
 
-const GTM_ID = 'G-DZZTFWZXD4';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Sobha Town Park', // IMPORTANT: Keep your actual site name
+    url: 'https://www.sobha-townparkproject.com/', // IMPORTANT: Replace with your actual domain
+    potentialAction: {
+      '@type': 'SearchAction',
+      target: {
+        '@type': 'EntryPoint',
+        urlTemplate: 'https://www.sobha-townparkproject.com/?s={search_term_string}', // IMPORTANT: Replace with your actual domain
+      },
+      'query-input': 'required name=search_term_string',
+    },
+  };
+
   return (
     <Html lang="en">
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
-          }}
-        />
-
+        {/* Sitelinks Structured Data */}
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'https://schema.org',
-              '@type': 'WebSite',
-              'name': 'Sobha Town Park',
-              'url': 'https://www.sobha-townparkproject.com/', // IMPORTANT: Replace with your actual domain
-              'potentialAction': {
-                '@type': 'SearchAction',
-                'target': {
-                  '@type': 'EntryPoint',
-                  'urlTemplate': 'https://www.sobha-townparkproject.com/?s={search_term_string}' // IMPORTANT: Replace with your actual domain
-                },
-                'query-input': 'required name=search_term_string',
-              },
-            }),
-          }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-        {/* --- End Sitelinks Structured Data --- */}
+        {/* End Sitelinks Structured Data */}
       </Head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {/* The GTM noscript tag that was here has been removed. */}
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
-
